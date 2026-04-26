f6e0b7daf27e7e805077af006f08cadf31600d3a
Arpan Mahara <amaha038@fiu.edu>
2026-04-25 21:36:31 -0400

    supporting ema state

---
+++ scflow/training/ema.py
@@ -0,0 +1,7 @@
+def ema(source, target, decay):
+    source_dict = source.state_dict()
+    target_dict = target.state_dict()
+    for key in source_dict.keys():
+        target_dict[key].data.copy_(
+            target_dict[key].data * decay + source_dict[key].data * (1.0 - decay)
+        )
